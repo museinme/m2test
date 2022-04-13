51c47da1953c14708b95094db5d499a7767e9937
alexander_bakhir <alexander.bakhir@overdose.digital>
2022-04-13 22:51:35 +0300

    test-task comment: theme create, styles & layout update for homepage product widget

---
+++ frontend/VendorName/newTheme/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Overdose Digital. All rights reserved.
+ * See LICENSE_OVERDOSE.txt for license details.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/VendorName/newTheme', __DIR__);
